690ec8b1267a8de4d3bcabee1c7b7ec6455ca7ff
Crafton Williams <crafton.williams@qut.edu.au>
2018-09-30 21:37:57 +1000

    I don't need an approval workflow

--- src/main/kotlin/com/crafton/scopepolicyserver/controllers/ApplicationController.kt
+++ src/main/kotlin/com/crafton/scopepolicyserver/controllers/ApplicationController.kt
@@ -2,48 +2,48 @@ package com.crafton.scopepolicyserver.controllers
 
 import com.crafton.scopepolicyserver.models.Application
 import com.crafton.scopepolicyserver.models.Policy
-import com.crafton.scopepolicyserver.services.ApplicationApprovalService
+import com.crafton.scopepolicyserver.repositories.ApplicationRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 
 @RestController
-class ApplicationController(private val applicationApprovalService: ApplicationApprovalService) {
+class ApplicationController(private val applicationRepository: ApplicationRepository) {
 
     @GetMapping("/applications")
-    fun getAllApplications() = applicationApprovalService.findAllApplications()
+    fun getAllApplications() = applicationRepository.findAll()
 
     @GetMapping("/applications/{id}")
-    fun getApplication(@PathVariable("id") applicationId: String) = applicationApprovalService.findApplicationById(applicationId)
+    fun getApplication(@PathVariable("id") applicationId: String) = applicationRepository.findById(applicationId)
             .map { ResponseEntity.ok(it) }
             .defaultIfEmpty(ResponseEntity.notFound().build())
 
     @PostMapping("/applications")
-    fun createApplication(@RequestBody application: Application) = applicationApprovalService.upsertApplication(application)
+    fun createApplication(@RequestBody application: Application) = applicationRepository.save(application)
 
     @PutMapping("/applications/{id}")
-    fun updateApplication(@PathVariable("id") applicationId: String, @RequestBody application: Application) = applicationApprovalService.findApplicationById(applicationId)
+    fun updateApplication(@PathVariable("id") applicationId: String, @RequestBody application: Application) = applicationRepository.findById(applicationId)
             .flatMap { existingApp ->
                 existingApp.name = application.name
                 existingApp.baseUrl = application.baseUrl
                 existingApp.policies = application.policies
-                applicationApprovalService.upsertApplication(existingApp)
+                applicationRepository.save(existingApp)
             }.map { ResponseEntity.ok(it) }
             .defaultIfEmpty(ResponseEntity.notFound().build())
 
     @PatchMapping("/applications/{id}/policy")
-    fun addPolicy(@PathVariable("id") applicationId: String, @RequestBody policy: Policy) = applicationApprovalService.findApplicationById(applicationId)
+    fun addPolicy(@PathVariable("id") applicationId: String, @RequestBody policy: Policy) = applicationRepository.findById(applicationId)
             .flatMap { existingApp ->
                 existingApp.policies?.add(policy)
-                applicationApprovalService.upsertApplication(existingApp)
+                applicationRepository.save(existingApp)
             }.map { ResponseEntity.ok(it) }
             .defaultIfEmpty(ResponseEntity.notFound().build())
 
     @DeleteMapping("/applications/{id}")
-    fun deletePolicy(@PathVariable("id") applicationId: String) = applicationApprovalService.findApplicationById(applicationId)
+    fun deletePolicy(@PathVariable("id") applicationId: String) = applicationRepository.findById(applicationId)
             .flatMap { existingApp ->
-                applicationApprovalService.removeApplication(existingApp)
+                applicationRepository.save(existingApp)
                         .then(Mono.just(ResponseEntity<Void>(HttpStatus.NO_CONTENT)))
             }
             .defaultIfEmpty(ResponseEntity.notFound().build())
--- src/main/kotlin/com/crafton/scopepolicyserver/repositories/ApprovalRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.crafton.scopepolicyserver.repositories
-
-import com.crafton.scopepolicyserver.models.Approval
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository
-
-interface ApprovalRepository : ReactiveMongoRepository<Approval, String>
